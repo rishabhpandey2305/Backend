ae3b3e7fdfea03252279fd8de4c1fee6bb2e6470
rishabhp <rishabhp@hyperlinkinfosystem.net.in>
2025-09-12 17:55:09 +0530

    Add success and error static methods to ApiResponse class; implement toJSON method for structured response output

--- utils/apiResponse.js
+++ utils/apiResponse.js
@@ -5,6 +5,27 @@ class ApiResponse {
     this.message = message;
     this.success = statusCode < 400;
   }
+
+  static success(data, message = "Success", statusCode = 200) {
+    return new ApiResponse(statusCode, data, message);
+  }
+
+  static error(
+    message = "Something went wrong",
+    statusCode = 500,
+    data = null
+  ) {
+    return new ApiResponse(statusCode, data, message);
+  }
+
+  toJSON() {
+    return {
+      statusCode: this.statusCode,
+      data: this.data,
+      message: this.message,
+      success: this.success,
+    };
+  }
 }
 
 export { ApiResponse };
