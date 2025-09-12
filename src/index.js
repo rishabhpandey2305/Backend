932f8da73fb8692d15aa677be7d78a0a98eb562c
rishabhp <rishabhp@hyperlinkinfosystem.net.in>
2025-09-12 17:41:37 +0530

    Add cookie-parser and cors as dependencies in package.json; refactor server startup logic in index.js and remove unused main.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+const port = process.env.PORT || 2000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is started at port: ${port}`);
+    });
+  } catch (error) {
+    console.error("MongoDB failed to connect!", error);
+    process.exit(1);
+  }
+};
+
+startServer();
